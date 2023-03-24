2ab1a602c1f436c64bd7abf09b7c6a8b35ede530
TikoGrantYT <admin@tikogrant.com>
2023-03-23 19:03:04 -0700

    commit

---
+++ script.js
@@ -0,0 +1,51 @@
+var currentCharacter = null;
+var gameActive = false;
+var interval = null;
+var intervalTime = 10;
+var score = 0;
+
+function hideAll() {
+    document.querySelectorAll(".screen").forEach(index => {
+        index.hidden = true;
+    });
+}
+window.addEventListener("load", () => {
+    hideAll();
+    document.querySelector(".welcome").hidden = false;
+});
+
+document.querySelector(".startBtn").addEventListener("click", () => {
+    hideAll();
+    document.querySelector(".game").hidden = false;
+    gameActive = true;
+    newKey();
+});
+
+function newKey() {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    currentCharacter = randomCharacter;
+    document.querySelector(".letter").innerHTML = currentCharacter;
+    interval = setTimeout(lose, intervalTime * 1000);
+    intervalTime = intervalTime - 0.2;
+}
+
+document.addEventListener("keypress", (e) => {
+    if (gameActive == false) return false;
+    if (/[a-z]/.test(e.key)) {
+        if (e.key == currentCharacter) {
+            clearTimeout(interval);
+            newKey();
+            score = score + 1;
+        } else {
+            lose();
+        }
+    }
+});
+
+function lose() {
+    hideAll();
+    document.querySelector(".lose").hidden = false;
+    document.querySelector(".score").innerHTML = score;
+    gameActive = false;
+}
